5c9c609e831c51efaa3424d3f7f652f1e7bf3fdb
vadik <v.tswts@yandex.ru>
2019-11-04 20:35:02 +0400

    add base structur and plus test table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+     <AppBaseSrtucture/>
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import AppBaseSrtucture from '@/components/AppBaseSrtucture.vue'
 
 export default {
   name: 'home',
   components: {
-    
+    AppBaseSrtucture
   }
 }
 </script>
