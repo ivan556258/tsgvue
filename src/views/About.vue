30c5744d1cacd5eab40c5bf254d80e8d78a46db9
vadik <v.tswts@yandex.ru>
2019-11-10 11:16:49 +0400

    remove conflict

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
     <h1>This is an about sfsdfpage</h1>
-=======
-    <h1>This is an about page</h1>
->>>>>>> 8be2af0e64ec04e405935340c8a553ee1a569f35
   </div>
 </template>
